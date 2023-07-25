f46fdf03aae3d335a9928e2bb20bd87a4b51189d
abrar_sajeel <abrarsajeel@gmail.com>
2023-07-25 23:47:25 +0500

    Added basic exercise list cards

--- workout-app/screens/Excercises.js
+++ workout-app/screens/Excercises.js
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
+import {Card, Avatar, Searchbar} from 'react-native-paper'
+
+//NOTE: temporary placeholder excercises
+const exercises = [
+    {
+        id: '1',
+        name: 'Bench Press',
+        muscleGroup: 'Chest',
+        description: 'Lie flat on a bench and push the bar up',
+        equipment: 'bench, barbell, weight plates',
+        gifUrl: 'www.abc.com'
+    },
+    {
+        id: '2',
+        name: 'Squat',
+        muscleGroup: 'Legs',
+        description: 'Hold barbell on traps and squat down and lift back up',
+        equipment: 'squat rack, barbell, weight plates',
+        gifUrl: 'www.abc.com'
+    }
+]
 
 function Excercises() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Excercises</Text>
+    <View style={styles.container}>
+        <Searchbar placeholder='Search for exercise'/>
+        <View style={styles.exerciseListContainer}>
+            {exercises.map((exercise) => (
+                <Card mode='elevated' style={styles.exerciseCard}>
+                    <Card.Title
+                        key={exercise.id}
+                        title={exercise.name}
+                        titleStyle={styles.exerciseName}
+                        subtitle={exercise.muscleGroup}
+                        left={(props) => <Avatar.Image {...props} icon="folder" />}
+                    />
+                </Card>
+            ))}
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        paddingTop: 50,
+    },
+    exerciseListContainer: {
+        paddingTop: 20
+    },
+    exerciseCard: {
+        marginBottom: 8,
+        borderRadius: 8,
+        backgroundColor: '#FFFFFF',
+    },
+    exerciseName: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
 
 export default Excercises
