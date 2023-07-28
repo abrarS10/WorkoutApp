9089b0ffa11d1347cdd0c7afe87f004797addde1
abrar_sajeel <abrarsajeel@gmail.com>
2023-07-28 22:39:34 +0500

    Reworked bottom nav bar and screen structure

--- workout-app/App.js
+++ workout-app/App.js
@@ -6,10 +6,10 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from './screens/HomeScreen';
 import ExcerciseScreen from './screens/ExcerciseScreen';
-import Workouts from './screens/Workouts';
-import Progress from './screens/Progress';
+import FoodScreen from './screens/FoodScreen';
+import MyProfileScreen from './screens/MyProfileScreen';
+import SearchScreen from './screens/SearchScreen';
 import { PaperProvider } from 'react-native-paper';
-
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
@@ -31,7 +31,16 @@ export default function App() {
             }}
           />
           <Tab.Screen
-            name='Excercises'
+            name='Search'
+            component={SearchScreen}
+            options={{
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name="store-search" color={color} size={26}/>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Workouts'
             component={ExcerciseScreen}
             options={{
               tabBarIcon: ({color}) => (
@@ -40,20 +49,20 @@ export default function App() {
             }}
           />
           <Tab.Screen
-            name='Workouts'
-            component={Workouts}
+            name='Food'
+            component={FoodScreen}
             options={{
               tabBarIcon: ({color}) => (
-                <MaterialCommunityIcons name="bridge" color={color} size={26}/>
+                <MaterialCommunityIcons name="food" color={color} size={26}/>
               ),
             }}
           />
           <Tab.Screen
-            name='Progress'
-            component={Progress}
+            name='Me'
+            component={MyProfileScreen}
             options={{
               tabBarIcon: ({color}) => (
-                <MaterialCommunityIcons name="boombox" color={color} size={26}/>
+                <MaterialCommunityIcons name="face-man-profile" color={color} size={26}/>
               ),
             }}
           />
---
+++ workout-app/screens/MyProfileScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyProfileScreen = () => {
+  return (
+    <View>
+      <Text>MyProfileScreen</Text>
+    </View>
+  )
+}
+
+export default MyProfileScreen
